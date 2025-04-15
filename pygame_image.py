5d9c8321ca7927f1a1812acf13ce4d9df66c7446
takashi <c0a24254@edu.teu.ac.jp>
2025-04-15 15:52:15 +0900

    4回目

--- pygame_image.py
+++ pygame_image.py
@@ -9,19 +9,25 @@ def main():
     pg.display.set_caption("はばたけ！こうかとん")
     screen = pg.display.set_mode((800, 600)) #画面サイズ
     clock  = pg.time.Clock()
-    bg_img = pg.image.load("fig/pg_bg.jpg")
+    bg_img = pg.image.load("fig/pg_bg.jpg") #画像読込
     koka = pg.image.load("fig/3.png")
-    tmr = 0
+    koka = pg.transform.flip(koka,True,False) #画像反転
+    bg_img_han = pg.transform.flip(bg_img,True,False)
+    x = 0
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
 
-        screen.blit(bg_img, [0, 0]) #背景画貼付
+        screen.blit(bg_img, [x, -50]) #背景画貼付
         screen.blit(koka, [300, 200])
         
-        pg.display.update()
-        tmr += 1        
+        pg.display.update()     
         clock.tick(200)
+
+        x = x - 1
+
+        if x == -:
+            screen.blit(bg_img_han, [x, 0])
 
 
 if __name__ == "__main__":
