0b3777a38d92a08200fa92aa88c227eb2ead53f3
takashi <c0a24254@edu.teu.ac.jp>
2025-04-15 17:37:30 +0900

    a1

--- pygame_image.py
+++ pygame_image.py
@@ -16,33 +16,37 @@ def main():
 
     koka_r = koka.get_rect() #rect抽出
     koka_r.center = 300, 200 #中心座標
-
     tmr = 0
 
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
 
+        tmr += 1
+        x = -(tmr % 3200)
+        y = -tmr
+
         key = pg.key.get_pressed()
         if key[pg.K_UP]:
             koka_r.move_ip(0,-1)
-
-        tmr += 1
-        x = -(tmr % 3200)
+        if key[pg.K_DOWN]:
+            koka_r.move_ip(0,1)
+        if key[pg.K_RIGHT]:
+            koka_r.move_ip(1,0)
+        if key[pg.K_LEFT]:
+            koka_r.move_ip(-1,0)
+        if key[pg.K_LEFT] == False:
+            koka_r.move_ip(y-200,0)
 
         screen.blit(bg_img, [x, -50]) #背景画貼付
         screen.blit(bg_img_han, [x + 1600, -50])
         screen.blit(bg_img, [x + 3200, -50])
+
+        #koka_r.center = x + 300, 200 #中心座標
         screen.blit(koka, koka_r)
         
         pg.display.update()     
         clock.tick(200) #fps
-
-        #if x == -800:
-        #    screen.blit(bg_img_han, [x+1600, 0])
-
-        #if x == -3200:
-        #    x = 0
 
 
 if __name__ == "__main__":
