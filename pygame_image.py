05519c51dc24c9795850cf565f4d4af0c2528f86
takashi <c0a24254@edu.teu.ac.jp>
2025-04-15 16:15:54 +0900

    6回目

--- pygame_image.py
+++ pygame_image.py
@@ -13,22 +13,27 @@ def main():
     koka = pg.image.load("fig/3.png")
     koka = pg.transform.flip(koka,True,False) #画像反転
     bg_img_han = pg.transform.flip(bg_img,True,False)
-    x = 0
+    tmr = 0
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
 
+        tmr += 1
+        x = -(tmr % 3200)
+
         screen.blit(bg_img, [x, -50]) #背景画貼付
-        screen.blit(bg_img_han, [x+1600, -50])
+        screen.blit(bg_img_han, [x + 1600, -50])
+        screen.blit(bg_img, [x + 3200, -50])
         screen.blit(koka, [300, 200])
         
         pg.display.update()     
-        clock.tick(200)
-
-        x = x - 1
+        clock.tick(200) #fps
 
         #if x == -800:
         #    screen.blit(bg_img_han, [x+1600, 0])
+
+        #if x == -3200:
+        #    x = 0
 
 
 if __name__ == "__main__":
